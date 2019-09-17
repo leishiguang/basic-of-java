23f973369579d6fbaa83b813cc72483c019a7688
leishiguang <leishiguang@outlook.com>
2019-09-17 13:40:33 +0800

    Merge Two Sorted Lists,合并两个有序链表

---
+++ algorithm/src/main/java/demo/algorithm/leetcode/L21.java
@@ -0,0 +1,76 @@
+package demo.algorithm.leetcode;
+
+/**
+ * 21.Merge Two Sorted Lists,合并两个有序链表
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ *
+ * @author leishiguang
+ * @since v1.0
+ */
+public class L21 {
+
+    /**
+     * 递归写法
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    /**
+     * 非递归写法
+     */
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode tmp = new ListNode(0);
+        ListNode res = tmp;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tmp.next = l1;
+                l1 = l1.next;
+            } else {
+                tmp.next = l2;
+                l2 = l2.next;
+            }
+            tmp = tmp.next;
+        }
+        while (l1 != null) {
+            tmp.next = l1;
+            l1 = l1.next;
+            tmp = tmp.next;
+        }
+        while (l2 != null) {
+            tmp.next = l2;
+            l2 = l2.next;
+            tmp = tmp.next;
+        }
+        return res.next;
+    }
+
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
