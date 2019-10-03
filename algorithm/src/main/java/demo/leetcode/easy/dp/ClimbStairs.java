8c00d9e5bd769c942100dbe2adf0b208072843b6
leishiguang <leishiguang@outlook.com>
2019-10-03 23:58:34 +0800

    add climb stairs

---
+++ algorithm/src/main/java/demo/leetcode/easy/dp/ClimbStairs.java
@@ -0,0 +1,36 @@
+package demo.leetcode.easy.dp;
+
+import java.util.HashMap;
+
+/**
+ * 爬楼梯
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/23/dynamic-programming/54/
+ * <p>
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ *
+ * @author leishiguang
+ * @since v1.0
+ */
+public class ClimbStairs {
+
+    private HashMap<Integer,Integer> map = new HashMap<>();
+
+
+    /**
+     * 迭代实现动态规划
+     */
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (!map.containsKey(n)){
+            map.put(n,climbStairs(n - 1) + climbStairs(n - 2));
+        }
+        return map.get(n);
+    }
+}
