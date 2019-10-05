2c0a65a02dc493500dbedb0ef17b84addc704611
leishiguang <leishiguang@outlook.com>
2019-10-05 23:54:40 +0800

    add StrStr

---
+++ algorithm/src/main/java/demo/leetcode/easy/strings/StrStr.java
@@ -0,0 +1,11 @@
+package demo.leetcode.easy.strings;
+
+/**
+ * 实现 strStr()
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/38/
+ *
+ * @author leishiguang
+ * @since v1.0
+ */
+public class StrStr {
+}
