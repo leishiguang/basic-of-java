e0201a9ef6c2e6c5f5abfaa157f7b8ed817b1488
leishiguang <leishiguang@outlook.com>
2019-10-08 23:38:54 +0800

    add ValidSudoku

---
+++ algorithm/src/main/java/demo/leetcode/easy/array/ValidSudoku.java
@@ -0,0 +1,11 @@
+package demo.leetcode.easy.array;
+
+/**
+ * 有效的数独
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/30/
+ *
+ * @author leishiguang
+ * @since v1.0
+ */
+public class ValidSudoku {
+}
