f3d09449827aca66b7c8207062bdacf3c3d33b0a
leishiguang <leishiguang@outlook.com>
2019-10-09 13:15:27 +0800

    add Atoi

--- algorithm/src/main/java/demo/leetcode/easy/strings/Atoi.java
+++ algorithm/src/main/java/demo/leetcode/easy/strings/Atoi.java
@@ -8,4 +8,44 @@ package demo.leetcode.easy.strings;
  * @since v1.0
  */
 public class Atoi {
+
+    public static int myAtoi(String str) {
+        if ("".equals(str)) {
+            return 0;
+        }
+        long result = 0;
+        int index = 0;
+        int flag = 1;
+        char[] chars = str.toCharArray();
+        int length = chars.length;
+        while (index < length && chars[index] == ' ') {
+            index++;
+        }
+        if (index == length) {
+            return 0;
+        } else if (chars[index] == '+') {
+            index++;
+        } else if (chars[index] == '-') {
+            flag = -1;
+            index++;
+        }
+        while (index < length && chars[index] >= '0' && chars[index] <= '9') {
+            result = result * 10 + chars[index] - '0';
+            if (flag == 1) {
+                if (result >= Integer.MAX_VALUE) {
+                    return Integer.MAX_VALUE;
+                }
+            } else {
+                if (result - 1 > Integer.MAX_VALUE) {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            index++;
+        }
+        return (int) (result * flag);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myAtoi(" "));
+    }
 }
