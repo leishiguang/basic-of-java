36a2dc4a2f6978d233fec40cd670ad094a599f5d
leishiguang <leishiguang@outlook.com>
2019-10-12 21:13:30 +0800

    add ValidBst

---
+++ algorithm/src/main/java/demo/leetcode/easy/trees/ValidBst.java
@@ -0,0 +1,17 @@
+package demo.leetcode.easy.trees;
+
+/**
+ * 验证二叉搜索树
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/48/
+ *
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * - 节点的左子树只包含小于当前节点的数
+ * - 节点的右子树只包含大于当前节点的数
+ * - 所有左子树和右子树自身必须也是二叉搜索树
+ *
+ * @author leishiguang
+ * @since v1.0
+ */
+public class ValidBst {
+
+}
