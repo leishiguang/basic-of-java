40e01f714ad0ceda9cf229f8e0b42b3baf491415
leishiguang <leishiguang@outlook.com>
2019-10-17 22:49:13 +0800

    add sorted array to bst

---
+++ algorithm/src/main/java/demo/leetcode/easy/trees/SortedArrayToBST.java
@@ -0,0 +1,11 @@
+package demo.leetcode.easy.trees;
+
+/**
+ * 将有序数组转换为二叉搜索树
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/51/
+ *
+ * @author leishiguang
+ * @since v1.0
+ */
+public class SortedArrayToBST {
+}
