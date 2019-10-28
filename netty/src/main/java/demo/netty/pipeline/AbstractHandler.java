ba2bfa7c39324bda867c6903a8b0deca205316fa
leishiguang <leishiguang@outlook.com>
2019-10-28 22:35:37 +0800

    delete pipeline

--- netty/src/main/java/demo/netty/pipeline/AbstractHandler.java
+++
@@ -1,9 +0,0 @@
-package demo.netty.pipeline;
-
-public abstract class AbstractHandler {
-    /**
-     * 处理器，这个处理器就做一件事情，在传入的字符串中增加一个尾巴..
-     */
-    abstract void doHandler(HandlerChainContext handlerChainContext, Object arg0); // handler方法
-
-}
