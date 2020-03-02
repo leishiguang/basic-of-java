608ea8612b81812ebd336ebc958aa7ba5105c597
leishiguang <leishiguang@outlook.com>
2020-03-02 17:27:41 +0800

    modify ArrayPractice

--- algorithm/src/main/java/demo/nowcoder/hw/ArrayPractice.java
+++ algorithm/src/main/java/demo/nowcoder/hw/ArrayPractice.java
@@ -10,6 +10,7 @@ import java.util.*;
  */
 public class ArrayPractice {
 
+
   /**
    * 题目描述 明明想在学校中请一些同学一起做一项问卷调查，为了实验的客观性，他先用计算机生成了N个1到1000之间的随机整数（N≤1000），对于其中重复的数字，只保留一个，把其余相同的数去掉，不同的数对应着不同的学生的学号。然后再把这些数从小到大排序，按照排好的顺序去找同学做调查。请你协助明明完成“去重”与“排序”的工作(同一个测试用例里可能会有多组数据，希望大家能正确处理)。
    * <p>
@@ -55,12 +56,13 @@ public class ArrayPractice {
 
 
   /**
-   * 切最少的砖块
+   * 切最少的砖块，https://leetcode-cn.com/problems/brick-wall/
    */
-  public int leastBricks(List<List<Integer>> params) {
+  public int leastBricks(List<List<Integer>> wall) {
     HashMap<Integer, Integer> maxMap = new HashMap<>();
     int maxValue = 0;
-    for (List<Integer> param : params) {
+    int maxKey = 0;
+    for (List<Integer> param : wall) {
       int sum = 0;
       for (Integer p : param) {
         sum += p;
@@ -71,11 +73,14 @@ public class ArrayPractice {
         }
       }
     }
-    maxMap.put(params.size(), 0);
+    for (Map.Entry<Integer, Integer> entry : maxMap.entrySet()) {
+      maxKey = Math.max(maxKey, entry.getKey());
+    }
+    maxMap.put(maxKey, 0);
     for (Map.Entry<Integer, Integer> entry : maxMap.entrySet()) {
       maxValue = Math.max(maxValue, entry.getValue());
     }
-    int total = params.size();
-    return total - maxMap.get(maxValue);
+    int total = wall.size();
+    return total - maxValue;
   }
 }
--- algorithm/src/test/java/demo/nowcoder/hw/ArrayPracticeTest.java
+++ algorithm/src/test/java/demo/nowcoder/hw/ArrayPracticeTest.java
@@ -16,30 +16,59 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class ArrayPracticeTest {
 
-  private List<List<Integer>> cases;
-  private int result;
+  private List<List<List<Integer>>> cases;
+  private List<Integer> result;
 
   @BeforeEach
   void prepare() {
     cases = new ArrayList<>();
+    result = new ArrayList<>();
+
+    List<List<Integer>> case1 = new ArrayList<>();
     Integer[] sum = new Integer[]{1, 2, 2, 1};
     Integer[] sum1 = new Integer[]{3, 1, 2};
     Integer[] sum2 = new Integer[]{1, 3, 2};
     Integer[] sum3 = new Integer[]{2, 4};
     Integer[] sum4 = new Integer[]{3, 1, 2};
     Integer[] sum5 = new Integer[]{1, 3, 1, 1};
-    cases.add(Arrays.asList(sum));
-    cases.add(Arrays.asList(sum1));
-    cases.add(Arrays.asList(sum2));
-    cases.add(Arrays.asList(sum3));
-    cases.add(Arrays.asList(sum4));
-    cases.add(Arrays.asList(sum5));
-    result = 2;
+    case1.add(Arrays.asList(sum));
+    case1.add(Arrays.asList(sum1));
+    case1.add(Arrays.asList(sum2));
+    case1.add(Arrays.asList(sum3));
+    case1.add(Arrays.asList(sum4));
+    case1.add(Arrays.asList(sum5));
+    int result1 = 2;
+    cases.add(case1);
+    result.add(result1);
+
+    List<List<Integer>> case2 = new ArrayList<>();
+    sum = new Integer[]{1, 1};
+    sum1 = new Integer[]{2};
+    sum2 = new Integer[]{1, 1};
+    case2.add(Arrays.asList(sum));
+    case2.add(Arrays.asList(sum1));
+    case2.add(Arrays.asList(sum2));
+    int result2 = 1;
+    cases.add(case2);
+    result.add(result2);
+
+    List<List<Integer>> case3 = new ArrayList<>();
+    sum = new Integer[]{2};
+    sum1 = new Integer[]{2};
+    sum2 = new Integer[]{2};
+    case3.add(Arrays.asList(sum));
+    case3.add(Arrays.asList(sum1));
+    case3.add(Arrays.asList(sum2));
+    int result3 = 3;
+    cases.add(case3);
+    result.add(result3);
   }
 
   @Test
   public void go() {
     ArrayPractice arrayPractice = new ArrayPractice();
-    assertEquals(result, arrayPractice.leastBricks(cases));
+    for (int i = 0; i < cases.size(); i++) {
+      assertEquals((int) result.get(i), arrayPractice.leastBricks(cases.get(i)));
+    }
   }
 }
